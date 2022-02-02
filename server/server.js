c82f13d45afc0d0c07685aa9bddf3262cbce84e4
rico <rikardoan@gmail.com>
2022-02-02 00:09:58 -0500

    apollo

--- server/server.js
+++ server/server.js
@@ -17,5 +17,5 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 --Now listening on localhost:${PORT}`));
 });
